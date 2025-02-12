a78e3813b794bccfb75262f4ab136fa48c53b564
Kevin Rodrigo Guerrero Posso <kevin.guerrero@pacific-hs.com>
2025-02-11 22:29:25 -0500

    fix

--- services/cantidades.js
+++ services/cantidades.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function GET_cantidades() {
-    return axios.get(`${"https://3.92.204.35:8000/"}inventario/Cantidades/`);
+    return axios.get(`${"https://3.92.204.35/"}inventario/Cantidades/`);
 }
